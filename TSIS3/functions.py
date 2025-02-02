746256dad6365e1e57c1a8745e414ab1b83f7e12
ouwyex <adiz.12.7.09@gmail.com>
2025-02-02 15:41:27 +0500

    first commit

--- TSIS3/functions.py
+++ TSIS3/functions.py
@@ -3,3 +3,113 @@ def my_function():
 
 my_function()
 
+
+
+##
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+#
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+
+
+
+#If the number of arguments is unknown, add a * before the parameter name:
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+#The following example shows how to use a default parameter value. If we call the function without argument, it uses the default value:
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+#
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+
+#passing a list
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+
+
+# To let a function return a value, use the return statement:
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+#To specify that a function can have only positional arguments, add , / after the arguments:
+def my_function(x, /):
+  print(x)
+
+my_function(3)
+
+
+#To specify that a function can have only keyword arguments, add *, before the arguments:
+def my_function(*, x):
+  print(x)
+
+my_function(x = 3)
+
+
+"""
+Combine Positional-Only and Keyword-Only
+You can combine the two argument types in the same function.
+
+Any argument before the / , are positional-only, and any argument after the *, are keyword-only.
+"""
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8)
+
+
+#recursion
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
